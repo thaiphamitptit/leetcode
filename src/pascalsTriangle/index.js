c1761f264ad4a5114ce993c116d4f512fea6ec6d
thaiphamitptit <phamthai.itptit@gmail.com>
2024-02-22 18:32:49 +0700

    add problem solution - pascals triangle

---
+++ src/pascalsTriangle/index.js
@@ -0,0 +1,11 @@
+import { generate } from './pascalsTriangle.js'
+
+const app = () => {
+  const inputs = [1, 2, 3, 4, 5]
+  inputs.map((input) => {
+    console.log(`Case ${inputs.indexOf(input) + 1}`)
+    console.log(generate(input))
+  })
+}
+
+app()
